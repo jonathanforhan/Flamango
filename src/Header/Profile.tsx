2225c2376102f34e8df59c82d8b5084a1666d7e6
jonathanforhan <jonathan.forhan@gmail.com>
2023-05-13 16:39:46 -0400

    remove dead profile button, relative icon sizing

--- src/Header/Profile.tsx
+++ src/Header/Profile.tsx
@@ -2,7 +2,7 @@ import { Component } from "solid-js";
 import { Props } from "../App";
 
 const Profile: Component<Props> = (props) => {
-  return <div class={props.class}>Profile</div>;
+  return <div class={props.class}></div>;
 };
 
 export default Profile;
