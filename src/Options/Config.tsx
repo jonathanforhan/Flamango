611978a9b661d8c2dd9288b18bc4e8051e0ed2f0
jonathanforhan <jonathan.forhan@gmail.com>
2023-05-16 17:47:09 -0400

    GPLv3

--- src/Options/Config.tsx
+++ src/Options/Config.tsx
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2023 Jonathan Forhan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { Component, createSignal, onMount, Setter } from "solid-js";
 import styles from "./Options.module.css";
 
@@ -36,7 +53,7 @@ const Config: Component<ConfigProps> = (props) => {
           min="0"
           max="6"
           value={rounding()}
-          onInput={(x) => handleRounding(+x.target.value)}
+          onInput={(x: any) => handleRounding(+x.target.value)}
         />
         <div style={{ "text-align": "right" }}>
           {1 / Math.pow(10, rounding())}
@@ -45,7 +62,7 @@ const Config: Component<ConfigProps> = (props) => {
       <div class={styles.Scientific}>
         <input
           type="checkbox"
-          onChange={(e) => props.setScientific(e.target.checked)}
+          onChange={(e: any) => props.setScientific(e.target.checked)}
         />
         <div style={{ "margin-left": "0.8em" }}>Scientific Notation</div>
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
+/*
+Copyright (C) 2023  Jonathan Forhan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?"
   );
 }
 
